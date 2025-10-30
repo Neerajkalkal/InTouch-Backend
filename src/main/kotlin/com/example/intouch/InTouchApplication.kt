977a851b701f7068475696eb1e3c7a7b3d8a80c8
kalkalneeraj1@gmail.com <neeraj@Neerajs-MacBook-Air.local>
2025-10-30 18:11:32 +0530

    commit

---
+++ src/main/kotlin/com/example/intouch/InTouchApplication.kt
@@ -0,0 +1,11 @@
+package com.example.intouch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InTouchApplication
+
+fun main(args: Array<String>) {
+    runApplication<InTouchApplication>(*args)
+}
